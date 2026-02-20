00bf5fcc714e6b49e3ec92aa42823e5a01878df4
AKING_YT <70751468+amarhitranjanbiruly@users.noreply.github.com>
2026-02-20 19:11:19 +0530

    Update kk.lua

--- kk.lua
+++ kk.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/dpeed", true))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/dpeed", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/fly", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/tp", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/cpsep", true))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/tp", true))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/cpsep", true))()
