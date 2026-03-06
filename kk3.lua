59e8d6e7ec7e341a77586ed7be06111d6b52346a
AKING_YT <70751468+amarhitranjanbiruly@users.noreply.github.com>
2026-03-06 13:50:56 +0530

    Update kk3.lua

--- kk3.lua
+++ kk3.lua
@@ -1,11 +1,24 @@
+-- local targetGameId = 101993432229107
+
+-- if game.PlaceId ~= targetGameId then
+--     return
+-- end
+
+-- -- main loop
+-- repeat
+--     task.wait()
+--     -- your code here
+-- until not game:IsLoaded()
+
 local targetGameId = 101993432229107
+local scriptURL = "https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/101993432229107"
 
 if game.PlaceId ~= targetGameId then
     return
 end
 
--- main loop
-repeat
-    task.wait()
-    -- your code here
-until not game:IsLoaded()
+while task.wait(5) do
+    pcall(function()
+        loadstring(game:HttpGet(scriptURL))()
+    end)
+end
