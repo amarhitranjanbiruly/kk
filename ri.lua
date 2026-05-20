54ff308cd36f5c8f91deb7c16b9f3aec61881ea0
AKING_YT <70751468+amarhitranjanbiruly@users.noreply.github.com>
2026-05-20 17:25:13 +0530

    Update ri.lua

--- ri.lua
+++ ri.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/rival'))()
+--task.wait(2) -- wait 2 seconds  optinal
 loadstring(game:HttpGet('https://raw.githubusercontent.com/amarhitranjanbiruly/kk/refs/heads/main/LAG%20FIX.LUA'))()
